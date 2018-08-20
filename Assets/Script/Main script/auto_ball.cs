fe6f7441546a21cc3b082240383c01f8538b5ae6
ats.2727-ls10@ezweb.ne.jp <ats.2727-ls10@ezweb.ne.jp>
2018-08-20 11:02:15 +0900

    砲台の調整

---
+++ Assets/Script/Main script/auto_ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class auto_ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject Player = GameObject.FindWithTag("Player");
+        //弾を自機に向かせるようなプログラムを書く
+        //ifの判定式　半径r以内のプログラムの動作を終了させる。
+	}
+}
--- Assets/Script/Main script/battery1.cs
+++ Assets/Script/Main script/battery1.cs
@@ -3,23 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class battery1 : MonoBehaviour {
-
+    public GameObject Ball;
+    Animator anim;
 	// Use this for initialization
 	void Start () {
-		
-	}
+       
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        Quaternion t = this.transform.rotation;
+        float tp = t.eulerAngles.z ;
         GameObject Player = GameObject.FindWithTag("Player");
-
-	}
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
+        Debug.Log(tp);
+        
+        /*if (tp >= 45)
         {
-            Destroy(this.gameObject);
-        }
+            Instantiate(Ball.gameObject);
+            this.GetComponent<Animator>().speed = 0;
+
+        }*/
     }
+
+   /* private void OnTriggerEnter2D(Collider2D collision)
+    {
+      //  float  phai;
+        if (collision.gameObject.tag == "Player") {
+
+            this.GetComponent<Animator>().speed = 0;
+         //   phai = this.transform.localEulerAngles.z;//角度の格納
+          //  for (; phai < phai + 10; phai += 0.1f) 
+            
+                }
+    }*/
 }
