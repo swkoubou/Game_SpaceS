d85244ce7be2b22b8cefcee613a748aae6120ba8
ats.2727-ls10@ezweb.ne.jp <ats.2727-ls10@ezweb.ne.jp>
2018-08-20 17:28:41 +0900

    砲台の射出タイミング、スクロールの実装

---
+++ Assets/molmotto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class molmotto : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 po = transform.localEulerAngles;
+        po.z = 90;
+        this.transform.localEulerAngles = po;
+	}
+}
